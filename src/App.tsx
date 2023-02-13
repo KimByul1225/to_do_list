02d07928d219944e11e530af31d3b5df3e81201b
ByulKim <byulkim1225@gmail.com>
2023-02-13 21:00:45 +0900

    form 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import ToDoList from './TodoList';
+import ToDoList from './ToDoList';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -57,8 +57,7 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  /* background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}; */
+
   line-height: 1.2;
 }
 a {
@@ -71,7 +70,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <ToDoList />
+      <ToDoList/>
     </>
   );
 }
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function TodoList() {
+function ToDoList() {
+    const [toDo, setToDo] = useState("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+        const {currentTarget : {value}} = event;
+        setToDo(value);
+    }
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(toDo)
+    }
     return (
         <div>
-            
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text" 
+                    placeholder='To do'
+                    value={toDo}
+                    onChange={onChange}
+                />
+                <button>Add</button>
+            </form>
         </div>
     );
 }
 
-export default TodoList;
+export default ToDoList;
