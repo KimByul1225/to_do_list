db7ff3ecd1c86b4fe629955a148f817078b35273
ByulKim <byulkim1225@gmail.com>
2023-02-13 21:45:29 +0900

    react hook form설치 및 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,28 +1,63 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-function ToDoList() {
-    const [toDo, setToDo] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget : {value}} = event;
-        setToDo(value);
-    }
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(toDo)
-    }
+// function ToDoList() {
+//     const [toDo, setToDo] = useState("");
+//     const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+//         const {currentTarget : {value}} = event;
+//         setToDo(value);
+//     }
+//     const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         console.log(toDo)
+//     }
+//     return (
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input 
+//                     type="text" 
+//                     placeholder='To do'
+//                     value={toDo}
+//                     onChange={onChange}
+//                 />
+//                 <button>Add</button>
+//             </form>
+//         </div>
+//     );
+// }
+
+function ToDoList(){
+    const { register, watch } = useForm();
+    console.log(watch());
     return (
-        <div>
-            <form onSubmit={onSubmit}>
+            <form>
                 <input 
+                    {...register("email")}
                     type="text" 
-                    placeholder='To do'
-                    value={toDo}
-                    onChange={onChange}
+                    placeholder='Email'
                 />
-                <button>Add</button>
+                <br />
+                <input 
+                    {...register("name")}
+                    type="text" 
+                    placeholder='Name'
+                />
+                <br />
+                <input 
+                    {...register("password")}
+                    type="password" 
+                    placeholder='Password'
+                />
+                <br />
+                <input 
+                    {...register("passwordConfirm")}
+                    type="password" 
+                    placeholder='PasswordConfirm'
+                />
+                <button>button</button>
             </form>
-        </div>
     );
 }
+
 
 export default ToDoList;
