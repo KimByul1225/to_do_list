2802151e9602376033e1d690918ccea88cdc56be
ByulKim <byulkim1225@gmail.com>
2023-02-15 21:24:30 +0900

    validation 체크

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -25,35 +25,62 @@ import { useForm } from 'react-hook-form';
 //         </div>
 //     );
 // }
-
+type IFormData = {
+    errors: {
+            email: {
+            message: string;
+        };
+    };
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+};
 function ToDoList(){
-    const { register, watch, handleSubmit, formState } = useForm();
+    const { register, watch, handleSubmit, formState: {errors}} = useForm<IFormData>({
+        defaultValues: {
+            email: "@naver.com"
+        }
+    });
     //console.log(watch());
     const onValid = (data:any) => {
         console.log(data);
     }
-    console.log(formState.errors);
+   // console.log(errors);
 
     return (
             <form onSubmit={handleSubmit(onValid)}>
+                
                 <input 
-                    {...register("email", { required:true })}
+                    {...register("email", { 
+                        required: "Email은 필수입니다.",
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                            message: "naver이메일만 사용가능합니다."
+                        }
+                    })}
                     type="text" 
                     placeholder='Email'
                 />
+                <span>
+                    {errors.email?.message}
+                </span>
                 <br />
                 <input 
                     {...register("name", { 
-                        required:true, 
-                        minLength: 10 
+                        required: "이름을 적어주세요", 
+                        minLength: 5 
                     })}
                     type="text" 
                     placeholder='Name'
                 />
+                <span>
+                    {errors.name?.message}
+                </span>
                 <br />
                 <input 
                     {...register("password", { 
-                        required:true,
+                        required: "비밀번호를 입력하세요",
                         minLength: {
                             value : 5,
                             message: "비밀번호가 너무 짧습니다."
@@ -62,15 +89,24 @@ function ToDoList(){
                     type="password" 
                     placeholder='Password'
                 />
+                <span>
+                    {errors.password?.message}
+                </span>
                 <br />
                 <input 
                     {...register("passwordConfirm", {
                         required: "비밀번호를 입력하세요",
-                        minLength: 5 
+                        minLength: {
+                            value : 5,
+                            message: "비밀번호가 너무 짧습니다."
+                        }
                     })}
                     type="password" 
                     placeholder='PasswordConfirm'
                 />
+                <span>
+                    {errors.passwordConfirm?.message}
+                </span>
                 <button>button</button>
             </form>
     );
