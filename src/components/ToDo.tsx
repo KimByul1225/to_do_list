c16c72dc799844124fe3d283f0100ee522923131
ByulKim <byulkim1225@gmail.com>
2023-02-18 23:20:32 +0900

    update기능

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,11 +2,51 @@ import React from 'react';
 import { useSetRecoilState } from 'recoil';
 import { IToDo, toDoState } from '../atoms';
 
+// [
+//     {
+//         "text": "1",
+//         "id": 1676728866655,
+//         "category": "TO_DO"
+//     },
+//     {
+//         "text": "2",
+//         "id": 1676728867956,
+//         "category": "TO_DO"
+//     },
+//     {
+//         "text": "3",
+//         "id": 1676728868985,
+//         "category": "TO_DO"
+//     },
+//     {
+//         "text": "4",
+//         "id": 1676728869960,
+//         "category": "TO_DO"
+//     },
+//     {
+//         "text": "5",
+//         "id": 1676728870910,
+//         "category": "TO_DO"
+//     }
+// ]
+
 function ToDo({text, category, id}: IToDo) {
-    const setToDos = useSetRecoilState(toDoState)
+    const setToDos = useSetRecoilState(toDoState);
+
     const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         const {currentTarget:{name}} = e;
-        console.log(name)
+        //console.log(name);
+
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newToDo = {text, id, category: name};
+            console.log("oldToDo", oldToDo);
+            console.log("newToDo", newToDo);
+            //console.log("targetIndex", targetIndex)
+            return oldToDos;
+        })
+
     }
     return (
         <li>
